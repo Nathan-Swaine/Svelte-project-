a95297989bd7aae22240eb67550d7da9fcb1e59b
nathan-parallax <54409127+nathan-parallax@users.noreply.github.com>
2021-01-21 21:29:17 +0000

    deleted app.svelete and replaced it. Made the background color grey and saved a link to a tutorial

---
+++ my-svelte-project/src/base.svelte
@@ -0,0 +1,20 @@
+<script>
+	//https://flaviocopes.com/svelte-import-components/
+</script>
+
+<main>
+	
+</main>
+
+<style>
+	main {
+		
+		width:100%; 
+		height:100%;
+		background-color: #121212;
+	}
+
+	
+
+	
+</style>
